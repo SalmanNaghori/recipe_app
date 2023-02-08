8fe168979e70fd1e41a9b952bf3e14b6e94f2209
SalmanNaghori <salmannaghori@gmail.com>
2023-02-08 19:01:39 +0530

    first commit

---
+++ lib/category_item.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/animation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:recipe_app/Category_meals_Screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title, id;
+  final Color color;
+  const CategoryItem(
+    this.id,
+    this.title,
+    this.color,
+  );
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            color.withOpacity(0.7),
+            color,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,65 @@
+import 'package:recipe_app/model/category.dart';
+import 'package:flutter/material.dart';
+
+const Dummy_Categories = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Exotic',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Aslan',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Hamburgers',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c6',
+    title: 'BreakFast',
+    color: Colors.grey,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Ice Cream',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Snacks',
+    color: Colors.pinkAccent,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Healthy',
+    color: Colors.deepOrangeAccent,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Soup',
+    color: Colors.redAccent,
+  ),
+  Category(
+    id: 'c11',
+    title: 'Street Food',
+    color: Colors.cyan,
+  ),
+  Category(
+    id: 'c12',
+    title: 'daily food',
+    color: Colors.lightGreenAccent,
+  ),
+];
